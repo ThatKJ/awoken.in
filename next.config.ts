eadca056154154effdb96f41bee5d0a6b0b3baf4
Kirtan <kirtan12007@gmail.com>
2026-07-29 21:36:26 +0530

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    webpackMemoryOptimizations: true,
+    preloadEntriesOnStart: false,
+  },
 };
 
 export default nextConfig;
